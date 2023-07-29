fd97b1f0f3ecb7540ff83a1fd09ea8d6d76a299c
ordepMendes <pedrohh0012@hotmail.com>
2023-07-29 10:27:43 -0300

    feat: :sparkles: Estruturação de HBS

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
-const express   = require('express');
-const app       = express();
-const db        = require('./database/connection');
-const bodyParse = require('body-parser');
-const router = express.Router();
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+const path = require('path');
+const db = require('./database/connection');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
-
-app.listen(PORT, function(){
-    console.log(`Rodando na porta: ${PORT}`);
+app.listen(PORT, function () {
+  console.log(`Rodando na porta: ${PORT}`);
 });
 
-//body parser
-app.use(bodyParse.urlencoded({extended: false}));
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// ? DB connection
+// Handlebars configuration
+const hbs = exphbs.create({ defaultLayout: 'main', extname: '.hbs' });
+app.engine('.hbs', hbs.engine);
+
+app.set('view engine', '.hbs'); 
+app.set('views', path.join(__dirname, 'views'));
+
+// Static files configuration
+app.use(express.static(path.join(__dirname, 'public')));
+
+// DB connection
 db.authenticate()
-.then(() => {
-    console.log('conectou ao banco');
-})
-.catch(err => {
+  .then(() => {
+    console.log('Conectou ao banco');
+  })
+  .catch(err => {
     console.log('Ocorreu um erro ' + err);
-})
+  });
 
-
-// ? routes
-app.get('/', function(req,res){
-    res.send('get funcionando');
+// Routes
+app.get('/', function (req, res) {
+  res.render("index");
 });
 
-// * jobs routes
-
+// jobs routes
 app.use('/jobs', require('./routes/jobs'));
